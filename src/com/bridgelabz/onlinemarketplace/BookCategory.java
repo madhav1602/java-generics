98b7f1916b556b414fa81a5a3259f1ec76cfc5e9
madhav1602 <madhavaggarwal580@gmail.com>
2025-03-27 19:12:22 +0530

    [Madhav] Add: sub category class

---
+++ src/com/bridgelabz/onlinemarketplace/BookCategory.java
@@ -0,0 +1,22 @@
+package src.com.bridgelabz.onlinemarketplace;
+
+public class BookCategory extends Category {
+
+    public BookCategory(){
+        super("Books");
+    }
+}
+
+class ClothingCategory extends Category{
+
+    public ClothingCategory(){
+        super("Clothes");
+    }
+}
+
+class GadgetCategory extends Category{
+
+    public GadgetCategory(){
+        super("Gadgets");
+    }
+}
